e0e44e4db54796f80ec0b08213c79c3c622e7938
shubham-singh9310 <shubham.singh93100@gmail.com>
2025-01-28 11:47:09 +0530

    user_config.py

--- user_config.py
+++ user_config.py
@@ -1,1 +1,1 @@
-openai_key = "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"
+openai_key = " ============================= your open ai key ========================================================"
